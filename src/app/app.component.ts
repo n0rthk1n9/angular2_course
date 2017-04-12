6d468dba3f65a5b09ac3bf63ad984253771ab134
Jan Armbrust <jan.armbrust@gmx.de>
2017-04-12 16:34:01 +0200

    used interpolations to display content of variables and return values of functions on webpage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 
 // export class so that it can be imported from anywhere
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  {
+
+  // new string for storing username
+  private username: string = "angularpro"
+  // instance of interface User filled with data
+  private currentUser: User = { username: "angularpro", email: "angularpro@gmail.com" }
+
+  // getter for the username of instance of User interface
+  private getUsername():string {
+    return this.currentUser.username
+  }
+}
+
+// declaration of interface User with its propertys
+interface User {
+  username: string,
+  email: string
+}
