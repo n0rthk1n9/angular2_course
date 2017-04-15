aa8804348155fb46eb98b7eefb2f609da4d8c475
Jan Armbrust <jan.armbrust@gmx.de>
2017-04-15 12:01:58 +0200

    binding some javascript properties to html attributes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,21 +3,30 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   // url to html file to load when app starts
-  templateUrl: 'app/app.component.html'
+  templateUrl: 'app/app.component.html',
+  styles: ['.blue-bg{ background-color: blue; }']
 })
 
 // export class so that it can be imported from anywhere
 export class AppComponent  {
 
-  // new string for storing username
-  private username: string = "angularpro"
   // instance of interface User filled with data
   private currentUser: User = { username: "angularpro", email: "angularpro@gmail.com" }
 
-  // getter for the username of instance of User interface
-  private getUsername():string {
-    return this.currentUser.username
+  // variable for hiding email
+  private hideEmail: boolean = true
+
+  // variable for saving input state of input field
+  private disabledInput: boolean = true
+  // funtcion to return value of input state variable
+  private isDisabled(): boolean {
+    return this.disabledInput
   }
+
+  // variable for saving state of blue background color
+  private isBlueBg: boolean = true
+  // variable for storing background color
+  private bgColor: string = 'red'
 }
 
 // declaration of interface User with its propertys
