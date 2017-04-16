dd00ec785c6cded6bdede7e2bc90707486856ba4
Jan Armbrust <jan.armbrust@gmx.de>
2017-04-16 15:46:48 +0200

    binding some javascript function to a html attribute

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'my-app',
   // url to html file to load when app starts
   templateUrl: 'app/app.component.html',
-  styles: ['.blue-bg{ background-color: blue; }']
+  // styles: ['.blue-bg{ background-color: blue; }']
 })
 
 // export class so that it can be imported from anywhere
@@ -16,17 +16,24 @@ export class AppComponent  {
   // variable for hiding email
   private hideEmail: boolean = true
 
-  // variable for saving input state of input field
-  private disabledInput: boolean = true
-  // funtcion to return value of input state variable
-  private isDisabled(): boolean {
-    return this.disabledInput
+  // // variable for saving input state of input field
+  // private disabledInput: boolean = true
+  // // funtcion to return value of input state variable
+  // private isDisabled(): boolean {
+  //   return this.disabledInput
+  // }
+  //
+  // // variable for saving state of blue background color
+  // private isBlueBg: boolean = true
+  // // variable for storing background color
+  // private bgColor: string = 'red'
+
+  // change value of hideEmail
+  private toggle() {
+    this.hideEmail = !this.hideEmail
   }
 
-  // variable for saving state of blue background color
-  private isBlueBg: boolean = true
-  // variable for storing background color
-  private bgColor: string = 'red'
+
 }
 
 // declaration of interface User with its propertys
