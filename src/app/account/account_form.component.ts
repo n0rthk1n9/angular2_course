62998485032c4020f9f480d23b48caca84ad163d
Jan Armbrust <jan.armbrust@gmx.de>
2017-05-12 16:59:56 +0200

    first steps to separate all form related stuff (forgot some files)

---
+++ src/app/account/account_form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Account } from './account.model';
+
+@Component({
+  selector: 'account-form',
+  templateUrl: 'app/account/account_form.component.html',
+  styleUrls: ['app/account/account_form.component.css']
+})
+
+export class AccountForm {
+
+}
